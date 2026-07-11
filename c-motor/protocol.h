373ae37ce50f6333dab2365da5dd9928f9ba40ad
Matheus Nascimento <matheusgamarra009@gmail.com>
2026-07-11 06:27:58 -0400

    feat(protocol): criar protocolo de comunicação através de uma struct
    - Inclusão da biblioteca <stdint.h> para garantir tipos com largura de bytes fixa.
    - Uso de __attribute__((packed)) para evitar o padding de bytes feito pelo compilador.
    - Variáveis nomeadas de acordo com as convenções de nomenclatura de tipos de dados em C.

---
+++ c-motor/protocol.h
@@ -0,0 +1,13 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#include <stdint.h>
+
+typedef struct __attribute__((packed)) {
+    uint8_t op_code;
+    uint8_t access_level;
+    char full_name[100];
+    char email[100];
+} user_protocol_t;
+
+#endif
