1a6bc2a37d1bd5d7c4cc2403d5095fe37829c88c
Matheus Nascimento <matheusgamarra009@gmail.com>
2026-07-11 11:35:26 -0400

    feat(database): implementar função de inserção de usuário
    - Estrutura de dados definida através de Prepared Statement para garantir segurança indestrutível contra SQL Injection.
    - Ciclo de vida do statement protegido com encerramento e liberação de memória em caso de falhas intermediárias.
    - Desempenho máximo garantido através da configuração do campo bind.length, permitindo que o driver do MySQL transmita apenas o tamanho real das strings.

--- c-motor/database/db.c
+++ c-motor/database/db.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <mysql/mysql.h>
 
-static MYSQL * global_connection = NULL;
+#include "protocol.h"
+
+static MYSQL *global_connection = NULL;
 
 void db_init(void){
     global_connection = mysql_init(NULL);
@@ -15,4 +17,57 @@ void db_init(void){
 
 void db_close(void){
     mysql_close(global_connection);
+}
+
+int db_insert_user(const user_protocol_t *user){
+
+    MYSQL_STMT *stmt_user = mysql_stmt_init(global_connection);
+
+    if(stmt_user == NULL){
+        fprintf(stderr, "Error connecting to the database: %s ", mysql_error(global_connection));
+        return -1;
+    }
+
+    const char query[] = "INSERT INTO users(nome, email) VALUES(?, ?)";
+    unsigned long length = strlen(query);
+
+    unsigned long len_name = strlen(user->full_name);
+    unsigned long len_email = strlen(user->email);
+
+    MYSQL_BIND bind[2];
+
+    memset(bind, 0, sizeof(bind));
+
+    bind[0].buffer_type = MYSQL_TYPE_STRING;
+    bind[0].buffer = (char *)user->full_name;
+    bind[0].buffer_length = sizeof(user->full_name);
+    bind[0].length = &len_name;
+
+    bind[1].buffer_type = MYSQL_TYPE_STRING;
+    bind[1].buffer = (char *)user->email;
+    bind[1].buffer_length = sizeof(user->email);
+    bind[1].length = &len_email;
+
+    if(mysql_stmt_prepare(stmt_user, query, length)){
+        fprintf(stderr, "Error during preparation: %s ", mysql_stmt_error(stmt_user));
+        mysql_stmt_close(stmt_user);
+        return -1;
+    }
+
+    if(mysql_stmt_bind_param(stmt_user, bind)){
+        fprintf(stderr, "Bind error: %s ", mysql_stmt_error(stmt_user));
+        mysql_stmt_close(stmt_user);
+        return -1;
+    }
+
+    if(mysql_stmt_execute(stmt_user)){
+        fprintf(stderr, "Error executing query: %s ", mysql_stmt_error(stmt_user));
+        mysql_stmt_close(stmt_user);
+        return -1;
+    }
+
+    mysql_stmt_close(stmt_user);
+
+    return 0;
+
 }
--- c-motor/database/db.h
+++ c-motor/database/db.h
@@ -1,7 +1,11 @@
 #ifndef DB_H
 #define DB_H
 
+#include "protocol.h"
+
 void db_init(void);
 void db_close(void);
+
+int db_insert_user(const user_protocol_t *user);
 
 #endif
